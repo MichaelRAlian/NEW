4e2f500c6d66e73bc2fa7c261c7b76feb7dbaafe
MichaelRAlian <102036024+MichaelRAlian@users.noreply.github.com>
2022-04-10 22:50:16 +0800

    Update index.php

--- index.php
+++ index.php
@@ -1,13 +1,23 @@
-<!DOCTYPE html>
 <html>
-    <head>
-        <title>My Home Page</title>
-     </head>
-     <body>
-     <h1> Welcome to my website!</h1>
-     <ul>
-         <li><a href="index.php">HOME</a></li>
-         <li><a href="/GroupAct/index.html">MyWebpage</a></li>
-    </ul>
-    </body>
-</html>
+<head>
+      <title> FREEDOM </title>
+      <link rel="stylesheet" type="text/css" href="css/stylesheet.css">
+</head>
+<body>
+	<header>
+	<div class="main">
+	<div class="logo">
+
+	<ul>
+	    <li><a href="#">Home</a></li>
+	    <li><a href="#">Services</a></li>
+	    <li><a href="#">Gallery</a></li>
+	    <li><a href="#">About</a></li>
+	    <li><a href="#">Contact</a></li>
+	</ul>
+	</div>
+	<div class="title">
+	<hl>Everything is possible with God</hl>
+	</div>
+	</header>
+<
